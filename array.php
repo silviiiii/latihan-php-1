266f826b10c09104fb45c9647b61c26ff6cba879
silviiiii <silvianindya11@gmail.com>
2019-08-03 05:02:56 -0400

    array

---
+++ array.php
@@ -0,0 +1,10 @@
+<?php
+$b='jono';
+$a = [1,2,4, 'hello', 100, 200, 500, 800, 0, 'juned', $b];
+for ($i=0; $i < count($a); $i++) { 
+    echo 'index array ke- ' . $i . ' dengan data ' . $a[$i] . '<br>'; 
+}
+
+
+
+
